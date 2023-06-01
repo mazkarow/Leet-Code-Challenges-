186cc2ab267f62a69b0c83bf6d08b959a6042128
Murtaza Kanorwala <mkanorwala94@gmail.com>
2023-06-01 15:32:54 -0400

    M

---
+++ #2627.py
@@ -0,0 +1,27 @@
+#2627. Debounce
+
+function debounce(fn, t) {
+  let timeout;
+  
+  return function debouncedFunction(...args) {
+    clearTimeout(timeout); // Clear the previous timeout
+  
+    timeout = setTimeout(() => {
+      fn.apply(this, args); // Execute the provided function after the delay
+    }, t);
+  }
+}
+
+// Example usage
+const start = Date.now();
+
+function log() {
+  console.log(Date.now() - start);
+}
+
+const debouncedLog = debounce(log, 20);
+
+setTimeout(debouncedLog, 10); // cancelled
+setTimeout(debouncedLog, 20); // logs: 40
+setTimeout(debouncedLog, 50); // cancelled
+setTimeout(debouncedLog, 60); // logs: 80
